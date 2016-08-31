b4d69c3648e52d681e294d00f2deb666301cd0b7
Hilary Lewis <hilarylewis92@gmail.com>
2016-08-31 11:05:34 -0600

    Add url to href

--- scripts.js
+++ scripts.js
@@ -4,17 +4,14 @@ $('#createbtn').on('click', function () {
 
   var title = $('#title-input').val();
   var url = $('#url-input').val();
+  //  $('input[type= #url-input').val();
 
   if (title === "" || url === "") {
     alert('Error: add a title and url before creating a bookmark.');
     return;
   }
 
-  $('.bookmark-list').append('<li class="unread">' + title +'</li>').append('<li class="unread">' + url +'</li>');
-
-  $('.bookmark-list').append('<button type="button" id="readbtn">Unread</span></button>');
-
-  $('.bookmark-list').append('<button type="button" id="removebtn">Remove</button>');
+  $('.bookmark-list').append('<div class = "unread"><li class = "title">' + title +'</li><li class = "url"><a href = "'+url+'" target="_blank">' + url + '</a></li></div>' + '<button type="button" id="readbtn">Unread</button>' + '<button type="button" id="removebtn">Remove</button>');
 
   // $("input[type=text], textarea").val("");
   // $("input[type=url], textarea").val("");
@@ -24,7 +21,7 @@ $('#createbtn').on('click', function () {
 });
 
 $('#readbtn').on('click', function () {
-  $(this).siblings().toggleClass("read");
+  $('.unread').siblings().toggleClass("read");
 });
 
 $('#removebtn').on('click', function () {
