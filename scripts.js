a36f643c50640b3148259b80ea4776c6722257c6
Hilary Lewis <hilarylewis92@gmail.com>
2016-08-30 21:43:02 -0600

    Added append to jQuery create button

--- scripts.js
+++ scripts.js
@@ -1,12 +1,22 @@
 $('#createbtn').on('click', function () {
   var title = $('#title-input').val();
-  $('.title-list').append('<li>' + title + '</li>');
   var url = $('#url-input').val();
-  $('.url-list').append('<li>' + url + '</li>');
+  $('.bookmark-list').append('<li class="unread">' + title +  url + '</li>');
+
+  $('.bookmark-list').append('<button type="button" id="readbtn">Unread</span></button>');
+
+  $('.bookmark-list').append('<button type="button" id="removebtn">Remove</button>');
+
+  $("input[type=text], textarea").val("");
+  $("input[type=url], textarea").val("");
 
   // location.href = $(this).find('#url-input').attr('href');
 });
 
 $('#readbtn').on('click', function () {
+  $(this).siblings().toggleClass("unread");
+});
 
+$('#removebtn').on('click', function () {
+  $(this).siblings().remove();
 });
