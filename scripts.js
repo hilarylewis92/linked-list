73605173fa7b582381b6bb93fea029e396e86d16
Hilary Lewis <hilarylewis92@gmail.com>
2016-08-31 08:55:19 -0600

    Add error prompt when input fields are empty

--- scripts.js
+++ scripts.js
@@ -1,20 +1,30 @@
+
+
 $('#createbtn').on('click', function () {
+
   var title = $('#title-input').val();
   var url = $('#url-input').val();
-  $('.bookmark-list').append('<li class="unread">' + title +  url + '</li>');
+
+  if (title === "" || url === "") {
+    alert('Error: add a title and url before creating a bookmark.');
+    return;
+  }
+
+  $('.bookmark-list').append('<li class="unread">' + title +'</li>').append('<li class="unread">' + url +'</li>');
 
   $('.bookmark-list').append('<button type="button" id="readbtn">Unread</span></button>');
 
   $('.bookmark-list').append('<button type="button" id="removebtn">Remove</button>');
 
-  $("input[type=text], textarea").val("");
-  $("input[type=url], textarea").val("");
+  // $("input[type=text], textarea").val("");
+  // $("input[type=url], textarea").val("");
+
 
   // location.href = $(this).find('#url-input').attr('href');
 });
 
 $('#readbtn').on('click', function () {
-  $(this).siblings().toggleClass("unread");
+  $(this).siblings().toggleClass("read");
 });
 
 $('#removebtn').on('click', function () {
