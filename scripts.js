eb64ffe01d3bf866d4f6d961fa23b285a1b99547
Hilary Lewis <hilarylewis92@gmail.com>
2016-08-31 16:24:39 -0600

    Working remove and toggleclass read button

--- scripts.js
+++ scripts.js
@@ -1,9 +1,25 @@
+var removeButton = $('#readbtn');
+var markAsReadButton = $('#readbtn');
+var createButton = $('#createbtn');
 
+function toggleButton (){
+  if ($('#title-input').val() !== '' && $('#url-input').val() !== '') {
+    createButton.attr('disabled', false);
+  } else {
+    createButton.attr('disabled', true);
+  }
+}
+$('#title-input').on('keyup', function(){
+  toggleButton();
+});
+$('#url-input').on('keyup', function(){
+  toggleButton();
+});
 
 $('#createbtn').on('click', function () {
   var title = $('#title-input').val();
   var url = $('#url-input').val();
-$('#form')[0].reset();
+  $('#form')[0].reset();
 
   if (title === "" || url === "") {
     alert('Error: Please add both a title and URL before creating bookmark.');
@@ -15,20 +31,20 @@ $('#form')[0].reset();
 
   function makeBookmark (title, url) {
     return $('.bookmark-list').append(
-      '<div class = "unread">' +
-        '<li>' + title +'</li>' +
-        '<li><a href="' + url + '" target="_blank">' + url + '</li></a>' +
-        '<button type="button" id="readbtn"> Mark as Read </button>' +
-        '<button type="button" id="removebtn"> Remove </button>' +
-      '</div>'
+      '<li>' +
+        '<span>' + title +'</span>' +
+        '<span><a href="' + url + '" target="_blank">' + url + '</a></span>' +
+        '<button type="button" class="readbtn"> Mark as Read </button>' +
+        '<button type="button" class="removebtn"> Remove </button>' +
+      '</li>'
     );
   }
 });
 
-$('#readbtn').on('click', function () {
-  $('.unread').siblings().toggleClass("read");
+$('.bookmark-list').on('click','.readbtn',function () {
+  $(this).parent().toggleClass("read");
 });
 
-$('#removebtn').on('click', function () {
-  $(this).siblings().remove();
+$('.bookmark-list').on('click', '.removebtn', function () {
+  $(this).parent().remove();
 });
